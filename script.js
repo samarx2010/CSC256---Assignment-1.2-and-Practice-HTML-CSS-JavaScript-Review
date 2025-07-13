b00da212e307ccce1c2274d18fd777ba869e1c96
samarx2010 <samarx2010@hotmail.com>
2025-07-13 13:54:13 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const newElement = document.createElement('div'); // create text string constant
+newElement.textContent = "Name: Savannah Marx";  // Text content
+newElement.classList.add('myStyledElement'); // Adds the CSS class
+document.body.appendChild(newElement); // appends to index.html document
+
+const newElement2 = document.createElement('div'); //creates a new text on different line
+newElement2.textContent = "Major: Advertising Art"; // Text content
+newElement2.classList.add('myStyledElement'); // Adds the CSS class
+document.body.appendChild(newElement2); // appends to index.html document
+
+const newElement3 = document.createElement('div'); // creates new text on a different line
+newElement3.textContent = "UAT Email Address: SMARX86292@uat.edu"; //Text content
+newElement3.classList.add('myStyledElement'); // Adds the CSS class
+document.body.appendChild(newElement3); // appends to index.html document
+
+const newElement4 = document.createElement('div'); // creates new text on a different line
+newElement4.textContent = "Expected Graduation Date: September 2025"; //Text content
+newElement4.classList.add('myStyledElement'); // Adds the CSS class
+document.body.appendChild(newElement4); // appends to index.html document 
+ 
+
+document.body.style.backgroundColor = "lightblue"; // background color for webpage for index.html
